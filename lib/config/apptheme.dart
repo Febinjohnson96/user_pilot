91a3b149058fd32d3f7eaf8f9e5d7ec6ecc0e17d
Febin Johnson <febinnj1996@gmail.com>
2023-09-25 21:44:51 +0530

    added basic structure

---
+++ lib/config/apptheme.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final appTheme = ThemeData(
+    useMaterial3: true,
+      fontFamily: 'Poppins',
+      textTheme: const TextTheme(
+        displayLarge: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 26,
+        ),
+        displayMedium: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 26,
+        ),
+        displaySmall: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 24,
+        ),
+        headlineMedium: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 22,
+        ),
+        headlineSmall: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 18,
+        ),
+        titleLarge: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 16,
+        ),
+        bodyLarge: TextStyle(
+          fontWeight: FontWeight.w300,
+          fontSize: 17,
+        ),
+        bodyMedium: TextStyle(
+          fontWeight: FontWeight.w300,
+          fontSize: 14,
+        ),
+        titleSmall: TextStyle(
+          fontWeight: FontWeight.w400,
+          fontSize: 14,
+        ),
+        labelLarge: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 24,
+        ),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white,
+          border: OutlineInputBorder(
+              borderSide: const BorderSide(
+                // color: buttonColor
+              ),
+              borderRadius: BorderRadius.circular(12)),
+          focusedBorder: OutlineInputBorder(
+              borderSide: const BorderSide(
+                // color: buttonColor,
+              ),
+              borderRadius: BorderRadius.circular(12)),
+          enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(
+                // color: buttonColor
+              ),
+              borderRadius: BorderRadius.circular(12))));
+}
---
+++ lib/config/routes.dart
@@ -0,0 +1,4 @@
+import 'package:get/get.dart';
+import 'package:user_pilot/presentation/home/home_screen.dart';
+
+List<GetPage> approutes = [GetPage(name: '/', page: () => const HomeScreen())];
