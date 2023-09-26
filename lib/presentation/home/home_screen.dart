8e5b041e5917a92fe9f30f42fe97b42a52da4eb0
Febin Johnson <febinnj1996@gmail.com>
2023-09-27 00:55:32 +0530

    added assets and resizing

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:user_pilot/gen/assets.gen.dart';
+import 'package:user_pilot/widgets/app_scaffold.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AppScaffold(
+      child: Column(children: [
+        Stack(
+          children: [
+            SizedBox(
+              height: 200.h,
+              width: 600.w,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.asset(
+                  Assets.images.home.homeHeader.path,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
+        )
+      ]),
+    );
   }
 }
